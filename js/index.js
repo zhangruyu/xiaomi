dbc2601d3be1dd4f14b0901f2b938a0538e47ec1
zhangruyu <1582034460@qq.com>
2018-08-01 18:25:31 +0800

    0

--- js/index.js
+++ js/index.js
@@ -336,4 +336,60 @@ window.onload=function () {
     f3(sg);
     let tj=document.querySelector(".tj");
     f3(tj);
+
+
+    // 倒计时
+    
+
+    let Times=sg.querySelectorAll(".box1 .son");
+    djs();
+    setInterval(djs,1000);
+
+    function djs(){
+        let arr=TIME();
+        Times.forEach(function (v,i){
+            v.innerText=arr[i+2];
+        })
+    }
+
+    function TIME(){
+        let date=new Date(2018,6,27,18);
+        let time=new Date();
+        let times=Math.floor((date-time)/1000);
+        let arr=[];
+        let mth=Math.floor(times/(30*24*60*60));
+        if (mth<10){
+            mth="0"+mth;
+        }
+        arr.push(mth);
+
+        times%=(30*24*60*60);
+        let day=Math.floor(times/(24*60*60));
+        if (day<10){
+            day="0"+day;
+        }
+        arr.push(day);
+
+        times%=(24*60*60);
+        let hours=Math.floor(times/(60*60));
+        if (hours<10){
+            hours="0"+hours;
+        }
+        arr.push(hours);
+
+        times%=(60*60);
+        let m=Math.floor(times/60);
+        if (m<10){
+            m="0"+m;
+        }
+        arr.push(m);
+
+        let s=times%60;
+        if (s<10){
+            s="0"+s;
+        }
+        arr.push(s);
+
+        return arr;
+    }
 }
